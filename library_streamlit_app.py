b134ccc1c82659425848654f74da56ab371c47bb
liisarik <elzbetty@hotmail.com>
2023-12-20 05:44:05 +0200

    Added functionalities for the administrator

--- library_streamlit_app.py
+++ library_streamlit_app.py
@@ -19,6 +19,7 @@ def display_data(query):
     cursor.execute(query)
     data = cursor.fetchall()
     df = pd.DataFrame(data, columns=[column[0] for column in cursor.description])
+    print(df)
     return df
 
 # Streamlit app layout
@@ -63,7 +64,54 @@ def main():
         cards_df = display_data("SELECT * FROM \"DB_project_gr4\".\"libCard\"")
         st.dataframe(cards_df)
 
+        # Function to add a new student
+        st.subheader('Add New Student')
+
+        # Input fields for student details
+        first_name = st.text_input('First Name')
+        last_name = st.text_input('Last Name')
+            # ... other fields
+
+        if st.button('Add Student'):
+        # Insert new student into the database
+             #cursor.execute(
+                # "INSERT INTO \"DB_project_gr4\".\"libMember\" (\"fName\", \"lName\", ...) VALUES (%s, %s, ...)",
+                #(first_name, last_name, ...))
+            #conn.commit()
+            st.success('Student added successfully')
+
 # ... need to add sections for other functionalities (Books, Copies, Loans, ...)
+
+
+    # Function to activate/deactivate card status
+    st.subheader('Activate/Deactivate Card Status')
+
+    card_id = st.number_input('Enter Card ID')
+    status = st.radio('Select Status', ('Activate', 'Deactivate'))
+
+    if st.button('Update Status'):
+        new_status = True if status == 'Activate' else False
+        cursor.execute("UPDATE \"DB_project_gr4\".\"libCard\" SET \"status\" = %s WHERE \"cardID\" = %s", (new_status, card_id))
+        conn.commit()
+        st.success(f'Card {card_id} status updated successfully')
+
+    elif option == 'Catalog of Books':
+        st.header('Catalog of Books')
+        books_df = display_data("SELECT * FROM \"DB_project_gr4\".\"book\"")
+        st.dataframe(books_df)
+
+        # Function to add a new book
+        st.subheader('Add New Book')
+
+        title = st.text_input('Title')
+        author = st.text_input('Author')
+        # ... other fields
+
+        if st.button('Add Book'):
+            cursor.execute("INSERT INTO \"DB_project_gr4\".\"book\" (\"title\", \"author\", ...) VALUES (%s, %s, ...)",
+                           (title, author, ...))
+            conn.commit()
+            st.success('Book added successfully')
 
 
 if __name__ == '__main__':
